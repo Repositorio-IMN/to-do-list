1c2c21f31ebbbf81e36ba12850b6bd1d657869bf
MoisesHsilva12 <moihgm67@gmail.com>
2024-10-19 00:23:26 -0300

    feat: adicionando funcionalidade de checkbox da tarefa

--- public/js/models/addNewTask.js
+++ public/js/models/addNewTask.js
@@ -8,12 +8,21 @@ export default class AddNewTask {
     createElement() {
         const taskBox = document.createElement('div');
         taskBox.classList.add('description-box');
-        taskBox.textContent = this.task;
+
+        const checkBoxTask = document.createElement('input');
+        checkBoxTask.type = 'checkbox';
+        checkBoxTask.classList.add('checkbox-task');
+
+        const taskText = document.createElement('span');
+        taskText.textContent = this.task;
+        taskText.classList.add('task-text');
 
         const buttonRemove = document.createElement('button');
         buttonRemove.textContent = 'X';
         buttonRemove.classList.add('button-remove');
 
+        taskBox.appendChild(checkBoxTask);
+        taskBox.appendChild(taskText);
         taskBox.appendChild(buttonRemove);
 
         buttonRemove.addEventListener('click', () => {
